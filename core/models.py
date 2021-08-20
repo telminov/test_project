b4008620ab39dab89aced4b37a446ebaea057ad5
telminov <telminov@soft-way.biz>
2021-08-20 17:23:33 +0300

    разбирались с моделями

--- core/models.py
+++ core/models.py
@@ -1,9 +1,23 @@
 from django.db import models
 
 
+class Author(models.Model):
+    name = models.CharField('Имя', max_length=255, unique=True)
+    birth = models.DateField('Год рождения', null=True, blank=True)
+
+
 class Book(models.Model):
+    authors = models.ManyToManyField(Author,
+                                     null=True, blank=True, related_name='books')
     name = models.CharField('Название', max_length=255)
     pages = models.IntegerField('Количество страниц', null=True, blank=True)
 
+    class Meta:
+        ordering = ('name', )
+
     def __str__(self):
         return self.name
+
+
+class BookProperties(models.Model):
+    book = models.OneToOneField(Book, related_name='prop', on_delete=models.CASCADE)
