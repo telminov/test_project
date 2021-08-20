a36b03c34c1d51de9542cfd90f015b5b1a4cd22f
telminov <telminov@soft-way.biz>
2021-08-20 18:30:14 +0300

    эксперименты с шаблонами

--- core/views.py
+++ core/views.py
@@ -3,11 +3,26 @@ from django.views.generic import TemplateView, ListView, DetailView
 import core.models
 
 
-class IndexView(TemplateView):
+class TitleMixin:
+    title: str = None
+
+    def get_title(self) -> str:
+        return self.title
+
+    def get_context_data(self, **kwargs):
+        c = super().get_context_data()
+        c['title'] = self.get_title()
+        return c
+
+
+class IndexView(TitleMixin, TemplateView):
     template_name = 'core/index.html'
+    title = 'Главная'
 
 
-class BookList(ListView):
+class BookList(TitleMixin, ListView):
+    title = 'Книги'
+
     def get_queryset(self):
         queryset = core.models.Book.objects.all()
         name = self.request.GET.get('name')
@@ -16,6 +31,9 @@ class BookList(ListView):
         return queryset
 
 
-class BookDetail(DetailView):
+class BookDetail(TitleMixin, DetailView):
     queryset = core.models.Book.objects.all()
+
+    def get_title(self):
+        return str(self.get_object())
 
