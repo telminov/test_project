bde5fe4643a37ccd38b44e8c155707722e6864e9
telminov <telminov@soft-way.biz>
2021-08-24 16:58:26 +0300

    еще немного тествых тестов

--- core/tests.py
+++ core/tests.py
@@ -1,4 +1,6 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+
 from core import models
 
 
@@ -10,3 +12,25 @@ class BookModelTestCase(TestCase):
     def testStr(self):
         self.assertEqual(str(self.book), 'Test Book')
 
+
+class BookSearchTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.book1 = models.Book.objects.create(name='Test Book 1')
+        self.book2 = models.Book.objects.create(name='Test Book 2')
+
+    def testWithoutParams(self):
+        response = self.client.get(reverse('core:books'))
+        self.assertSequenceEqual(
+            list(response.context['object_list']),
+            list(models.Book.objects.all()),
+            'При поиске без параметров должны выводиться все книги',
+        )
+
+    def testSearchByName(self):
+        response = self.client.get(reverse('core:books'), data={'name': 'Test Book 1'})
+        self.assertEqual(1, response.context['object_list'].count())
+        self.assertEqual(
+            'Test Book 1',
+            response.context['object_list'].first().name,
+        )
